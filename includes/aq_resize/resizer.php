09cf84cbba02ac0e8e328d7907b3439ca2c22af8
aadiityaak <42263468+aadiityaak@users.noreply.github.com>
2022-01-03 20:57:15 +0700

    Fix error aq_resize again :(

--- includes/aq_resize/resizer.php
+++ includes/aq_resize/resizer.php
@@ -262,3 +262,42 @@ if ( ! function_exists( 'wp_img_resizer' ) ) {
 	}
 
 } // end if function exists
+
+
+if ( ! function_exists( 'aq_resize' ) ) {
+
+    /**
+     * Aqua Resizer
+     *
+     * @param string $url    - (required) must be uploaded using wp media uploader
+     * @param int    $width  - (required)
+     * @param int    $height - (optional)
+     * @param bool   $crop   - (optional) default to soft crop
+     * @param bool   $retina - (optional) default to no double of width and height
+     * @param bool   $single - (optional) returns an array if false
+     *
+     * @return array|string|WP_Error
+     */
+    function aq_resize( $url, $width = NULL, $height = NULL, $crop = NULL, $retina = FALSE, $single = TRUE ) {
+        
+        // get error message
+        if ( empty( $url ) )
+            return new WP_Error( 'no_image_url', __( 'No image URL has been entered.' ), $url );
+        
+        // use settings of WordPress
+        if ( empty( $width ) )
+            $width = get_option( 'thumbnail_size_w' );
+        
+        $args = array(
+            'url'    => $url,
+            'width'  => $width,
+            'height' => $height,
+            'crop'   => $crop,
+            'retina' => $retina,
+            'single' => $single
+        );
+        
+        return wp_img_resizer_src( $args );
+    }
+    
+} // end if function_exists
