268809994a1e1df23932a3767a7337db7883ed7e
aadiityaak <42263468+aadiityaak@users.noreply.github.com>
2022-02-26 17:55:34 +0700

    Remove unused function

--- sweetaddon.php
+++ sweetaddon.php
@@ -37,32 +37,6 @@ if ( ! defined( 'WPINC' ) ) {
  */
 define( 'SWEETADDON_VERSION', '1.0.0' );
 
-/**
- * The code that runs during plugin activation.
- * This action is documented in includes/class-sweetaddon-activator.php
- */
-function activate_sweetaddon() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sweetaddon-activator.php';
-	Sweetaddon_Activator::activate();
-}
-
-/**
- * The code that runs during plugin deactivation.
- * This action is documented in includes/class-sweetaddon-deactivator.php
- */
-function deactivate_sweetaddon() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-sweetaddon-deactivator.php';
-	Sweetaddon_Deactivator::deactivate();
-}
-
-register_activation_hook( __FILE__, 'activate_sweetaddon' );
-register_deactivation_hook( __FILE__, 'deactivate_sweetaddon' );
-
-/**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
- */
-require plugin_dir_path( __FILE__ ) . 'includes/class-sweetaddon.php';
 
 /**
  * Add aq resize function
