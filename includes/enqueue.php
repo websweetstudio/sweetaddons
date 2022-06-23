12923204a625ea3fbf336088732d7b2ba2393186
Aditya Kristyanto <42263468+aadiityaak@users.noreply.github.com>
2022-06-23 14:08:33 +0700

    Update enqueue.php

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -2,5 +2,5 @@
 wp_enqueue_style( 'sweetaddon-style', SWEETADDON_URL . 'assets/css/style.css', array(), '1.0.0', 'all' );
 wp_enqueue_style( 'sweetaddon-custom', SWEETADDON_URL . 'assets/css/custom.css', array(), '1.0.0', 'all' );
 
-wp_enqueue_script( 'sweetaddon-script', SWEETADDON_URL . 'assets/js/script.js', array( 'jquery' ), '1.0.0', true );
-wp_enqueue_script( 'sweetaddon-custom', SWEETADDON_URL . 'assets/js/custom.js', array( 'jquery' ), '1.0.0', true );
+wp_enqueue_script( 'sweetaddon-script', SWEETADDON_URL . 'assets/js/script.js', array( 'child-understrap-scripts' ), '1.0.0', true );
+wp_enqueue_script( 'sweetaddon-custom', SWEETADDON_URL . 'assets/js/custom.js', array( 'child-understrap-scripts' ), '1.0.0', true );
