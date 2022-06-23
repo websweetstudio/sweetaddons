1078c60dca746c5d3717a0e0875ea7f8294fa93d
Aditya Kristyanto <42263468+aadiityaak@users.noreply.github.com>
2022-06-23 14:45:17 +0700

    Update enqueue.php

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -1,6 +1,9 @@
 <?php
-wp_enqueue_style( 'sweetaddon-style', SWEETADDON_URL . 'assets/css/style.css', array(), '1.0.0', 'all' );
-wp_enqueue_style( 'sweetaddon-custom', SWEETADDON_URL . 'assets/css/custom.css', array(), '1.0.0', 'all' );
+function plugins_enqueue_styles() {
+  wp_enqueue_style( 'sweetaddon-style', SWEETADDON_URL . 'assets/css/style.css', array(), '1.0.0', 'all' );
+  wp_enqueue_style( 'sweetaddon-custom', SWEETADDON_URL . 'assets/css/custom.css', array(), '1.0.0', 'all' );
 
-wp_enqueue_script( 'sweetaddon-script', SWEETADDON_URL . 'assets/js/script.js', array( 'child-sweetchild-scripts' ), '1.0.0', true );
-wp_enqueue_script( 'sweetaddon-custom', SWEETADDON_URL . 'assets/js/custom.js', array( 'child-sweetchild-scripts' ), '1.0.0', true );
+  wp_enqueue_script( 'sweetaddon-script', SWEETADDON_URL . 'assets/js/script.js', array(), '1.0.0', true );
+  wp_enqueue_script( 'sweetaddon-custom', SWEETADDON_URL . 'assets/js/custom.js', array(), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'plugins_enqueue_styles', 20 );
