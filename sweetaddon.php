9e7cf678a90724e48b489b408c4d738ddcc74b2e
aadiityaak <aadiityaak@gmail.com>
2022-07-26 22:16:41 +0700

    Add npm build

--- sweetaddon.php
+++ sweetaddon.php
@@ -35,7 +35,7 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'SWEETADDON_VERSION', '1.0.0' );
+define( 'SWEETADDON_VERSION', '1.5.0' );
 
 /**
  * Define plugin path url
