9c0285e399dad9bd2fc73722878f42d687479ee6
Imam Tri <96063333+Iimamtzh@users.noreply.github.com>
2022-08-06 16:22:25 +0700

    fix thumbnail shortcode

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -21,7 +21,7 @@ function sweet_thumbnail_shortcode( $atts ) {
     $image_url = $image_url[0];
 
     ?>
-    <div class="sweet-thumbnail">
+    <div class="sweet-thumbnail-container">
         <a class="sweet-thumbnail <?php echo esc_attr( $class ).' ratio-'.$ratio; ?>" style="background-image:url(<?php echo $image_url; ?>);" <?php $link; ?>>
             <img style="display: none;" src="<?php echo $image_url; ?>" alt="<?php the_title(); ?>">
         </a>
