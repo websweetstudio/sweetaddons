222e270984fef5b0274f776f189fb39e6ccb618f
Iimamtzh <iimamtzhthriiee21@gmail.com>
2022-12-27 17:18:52 +0700

    Fix shorshode lazy load

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -1,16 +1,17 @@
 <?php
-function plugins_enqueue_styles() {
-  wp_enqueue_style( 'sweetaddon-style', SWEETADDON_URL . 'assets/css/sweetaddon.min.css', array(), '1.0.0', 'all' );
+function plugins_enqueue_styles()
+{
+  wp_enqueue_style('sweetaddon-style', SWEETADDON_URL . 'assets/css/sweetaddon.min.css', array(), '1.0.0', 'all');
 
   wp_enqueue_script('jQuery');
-  wp_enqueue_script( 'sweetaddon-script', SWEETADDON_URL . 'assets/js/sweetaddon.min.js', array(), '1.0.0', true );
+  wp_enqueue_script('lazy-script', 'https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.11/jquery.lazy.min.js', array(), '1.0.0', true);
+  wp_enqueue_script('sweetaddon-script', SWEETADDON_URL . 'assets/js/sweetaddon.min.js', array(), '1.0.0', true);
 
-  wp_add_inline_style( 'sweetaddon-style', get_theme_mod( 'custom_code_css' ) );
-  wp_add_inline_script( 'sweetaddon-script', get_theme_mod( 'custom_code_js' ) );
+  wp_add_inline_style('sweetaddon-style', get_theme_mod('custom_code_css'));
+  wp_add_inline_script('sweetaddon-script', get_theme_mod('custom_code_js'));
 
-  wp_localize_script( 'sweetaddon-script', 'sweetaddon', array( 
-    'ajaxurl' => admin_url( 'admin-ajax.php' )
+  wp_localize_script('sweetaddon-script', 'sweetaddon', array(
+    'ajaxurl' => admin_url('admin-ajax.php')
   ));
-
 }
-add_action( 'wp_enqueue_scripts', 'plugins_enqueue_styles', 20 );
+add_action('wp_enqueue_scripts', 'plugins_enqueue_styles', 20);
--- src/js/sweetaddon.js
+++ src/js/sweetaddon.js
@@ -1,23 +1,42 @@
-jQuery(function() {
-    // on scroll top
-    jQuery(window).scroll(function() {
-        if (jQuery(this).scrollTop() > 100) {
-            if (jQuery('.scroll-active').hasClass('scroll-active') && jQuery('.scroll-active').hasClass('right')) {
-                jQuery('.scroll-active').css('margin-right', '45px');
-            }
-            jQuery('.scroll-to-top').fadeIn();
-        } else {
-            if (jQuery('.scroll-active').hasClass('scroll-active') && jQuery('.scroll-active').hasClass('right')) {
-                jQuery('.scroll-active').css('margin-right', '0px');
-            }
-            jQuery('.scroll-to-top').fadeOut();
-        }
-    });
-    // on click scroll to top
-    jQuery('.scroll-to-top').click(function() {
-        jQuery('html, body').animate({
-            scrollTop: 0
-        }, 400);
-        return false;
-    });
+jQuery(function () {
+  // on scroll top
+  jQuery(window).scroll(function () {
+    if (jQuery(this).scrollTop() > 100) {
+      if (
+        jQuery(".scroll-active").hasClass("scroll-active") &&
+        jQuery(".scroll-active").hasClass("right")
+      ) {
+        jQuery(".scroll-active").css("margin-right", "45px");
+      }
+      jQuery(".scroll-to-top").fadeIn();
+    } else {
+      if (
+        jQuery(".scroll-active").hasClass("scroll-active") &&
+        jQuery(".scroll-active").hasClass("right")
+      ) {
+        jQuery(".scroll-active").css("margin-right", "0px");
+      }
+      jQuery(".scroll-to-top").fadeOut();
+    }
+  });
+  // on click scroll to top
+  jQuery(".scroll-to-top").click(function () {
+    jQuery("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      400
+    );
+    return false;
+  });
+  // LAZY LOAD SHORTCHODE
+  jQuery(".sweet-thumbnail").Lazy({
+    // your configuration goes here
+    scrollDirection: "vertical",
+    effect: "fadeIn",
+    visibleOnly: true,
+    onError: function (element) {
+      console.log("error loading " + element.data("src"));
+    },
+  });
 });
