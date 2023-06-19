93d0d4197a68cee55aa92befd3d6ec660c130c3b
aadiityaak <aadiityaak@gmail.com>
2023-06-19 15:28:19 +0700

    fix bug

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -1,11 +1,11 @@
 <?php
 function plugins_enqueue_styles()
 {
-  wp_enqueue_style('sweetaddons-style', SWEETADDON_URL . 'assets/css/sweetaddons.min.css', array(), '1.0.0', 'all');
+  wp_enqueue_style('sweetaddons-style', SWEETADDON_URL . 'public/css/sweeetaddons-public.min.css', array(), '1.0.0', 'all');
 
   wp_enqueue_script('jQuery');
   wp_enqueue_script('lazy-script', 'https://cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.11/jquery.lazy.min.js', array(), '1.0.0', true);
-  wp_enqueue_script('sweetaddons-script', SWEETADDON_URL . 'assets/js/sweetaddons.min.js', array(), '1.0.0', true);
+  wp_enqueue_script('sweetaddons-script', SWEETADDON_URL . 'public/js/sweetaddons.js', array(), '1.0.0', true);
 
   wp_add_inline_style('sweetaddons-style', get_theme_mod('custom_code_css'));
   wp_add_inline_script('sweetaddons-script', get_theme_mod('custom_code_js'));
